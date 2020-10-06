c17dd43b2944f018f28973277e1da95a2aafdff6
aarongoen <aarongoen2.0@gmail.com>
2020-10-06 11:36:44 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,5 +4,16 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+    # attr_accessor :id
+    
+    # def initialize(id=nil)
+    #    @id = id
+    # end 
 
+    self.column_names.each do |col_name| #Why don't I have to name the method? Is "attr_accessor" a keyword? Is that why it doesn't have an "=" afterwards?
+        attr_accessor col_name.to_sym
+    end
+
+    
+    
 end
